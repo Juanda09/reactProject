ad14255760b98e92bd361f4bbd78d50e6232974f
j58937672 <j58937672@gmail.com>
2024-03-15 18:18:30 -0500

    Redux

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,9 @@
 export default function Footer(){
 
     return (
-        <div className="bg-gray-800 text-white py-4 text-center bottom-0 w-full">
+        <div className="bg-gray-800 text-white py-4 text-center  bottom-0 w-full">
             <h1>Proyecto talento Tech</h1>
-            <h2 className="text-red-500">Desarrollador por Juan David Huertas Zapata</h2>
+            <h2 className="text-red-500">Desarrollado por Juan David Huertas Zapata</h2>
         </div>
     );
 }
