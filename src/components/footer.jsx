74761d0548f5e813e113ba534bf7b030a9bd61b8
j58937672 <j58937672@gmail.com>
2024-03-18 20:17:33 -0500

    Avance

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return (
         <div className="bg-gray-800 text-white py-4 text-center  bottom-0 w-full">
             <h1>Proyecto talento Tech</h1>
-            <h2 className="text-red-500">Desarrollado por Juan David Huertas Zapata</h2>
+            <h2 className="text-red-500">Desarrollador por Juanito</h2>
         </div>
     );
 }
