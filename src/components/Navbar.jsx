789d4f65fdacea3cf4003fe1c1c61bc6dcf63e4a
j58937672 <j58937672@gmail.com>
2024-03-29 09:32:03 -0500

    Avance

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  link: {
+    color: '#fff',
+    textDecoration: 'none',
+    marginRight: theme.spacing(2),
+    '&:hover': {
+      color: theme.palette.secondary.main,
+    },
+  },
+}));
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
-    <nav className="bg-gray-800 py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between">
-          <div className="flex space-x-4">
-            <Link to="/" className="text-white hover:text-gray-300">Inicio</Link>
-            <Link to="/userlist" className="text-white hover:text-gray-300">Lista de Usuarios</Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <AppBar position="static" className={classes.navbar}>
+      <Toolbar>
+        <Typography variant="h6">
+          <Link to="/" className={classes.link}>Inicio</Link>
+        </Typography>
+        <Typography variant="h6">
+          <Link to="/userlist" className={classes.link}>Lista de Usuarios</Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
